aab0df0e0c78d57220dacc9a041cb71811e86c59
Robb Irrgang <robb@irrg.org>
2015-07-21 11:44:26 -0500

    add support for instagram hashtags

--- lib/auto_html/filters/hashtag.rb
+++ lib/auto_html/filters/hashtag.rb
@@ -3,5 +3,7 @@ AutoHtml.add_filter(:hashtag).with(:source => :twitter) do |text, options|
     text.gsub(/#([^\s]+)/, '<a href="http://twitter.com/search?q=%23\1&f=realtime" class="hashtag" target="_blank">#\1</a>')
   elsif options[:source] == :facebook
     text.gsub(/#([^\s]+)/, '<a href="https://www.facebook.com/hashtag/\1" class="hashtag" target="_blank">#\1</a>')
+  elsif options[:source] == :instagram
+    text.gsub(/#([^\s]+)/, '<a href="https://instagram.com/explore/tags/\1" class="hashtag" target="_blank">#\1</a>')
   end
 end
